d4c5a68cc1af17c07e729b3928b52187737d138f
Prabhakar Maurya <mauryaprabhakar677@gmail.com>
2024-12-18 00:49:02 +0530

    Update server.config.js

--- Server/config/server.config.js
+++ Server/config/server.config.js
@@ -2,8 +2,8 @@ export const server={
 PORT:8080,
 DB_URL:'mongodb://localhost:27017/BUS_APP',
 Secret_code:'This is vary secrate',
-Gmailuser: "safarofficial4049@gmail.com",
-Gmailpass: "szrv xnzt ulwj qske"
+Gmailuser: "Your Gmail",
+Gmailpass: "Password"
 
 
 }
